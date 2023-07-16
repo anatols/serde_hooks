dd6b1303b2c1afb9336b6df123c4aea06eceb0bf
Anatoliy Samara <>
2023-07-16 20:02:33 +0200

    Test for `into_static_str`

--- src/ser/wrapper/struct_wrapper.rs
+++ src/ser/wrapper/struct_wrapper.rs
@@ -97,14 +97,16 @@ impl<'h, S: Serializer, H: SerializerWrapperHooks> serde::ser::SerializeStruct
         let res = if skip_field {
             self.serialize_struct.skip_field(key)
         } else if let Some(replacement_value) = replacement_value {
-            self.serialize_maybe_renamed_field(field_key, &replacement_value)
+            self.serialize_struct
+                .serialize_field(into_static_str(field_key), &replacement_value)
         } else {
             let s = SerializableWithHooks {
                 serializable: value,
                 hooks: self.hooks,
                 kind: SerializableKind::Value,
             };
-            self.serialize_maybe_renamed_field(field_key, &s)
+            self.serialize_struct
+                .serialize_field(into_static_str(field_key), &s)
         };
 
         self.hooks.path_pop();
@@ -117,55 +119,84 @@ impl<'h, S: Serializer, H: SerializerWrapperHooks> serde::ser::SerializeStruct
     }
 }
 
-impl<'h, S: Serializer, H: SerializerWrapperHooks> SerializeStructWrapper<'h, S, H> {
-    fn serialize_maybe_renamed_field<T: ?Sized>(
-        &mut self,
-        key: Cow<'static, str>,
-        value: &T,
-    ) -> Result<(), S::Error>
-    where
-        T: Serialize,
-    {
-        // Serde expects struct fields to be `&'static str` because for structs the field
-        // names are knows at compile time. A serializer can theoretically hold on to those
-        // field name references forever and expect them to be valid.
-        //
-        // To be able to rename a field, we thus need to somehow generate a string at
-        // runtime that will have a 'static lifetime.
-        //
-        // The 'static lifetime is defined as 'will live till the program ends'. Here
-        // we keep a static set of pinned Box<str>, and store unique names in it.
-        // The static set will not get destroyed until the program ends. Although
-        // boxes can move in the set, where they point remains pinned in place until the end
-        // of the program because we never delete. For all practical purposes those boxed
-        // strs are static, so safe to transmute to 'static lifetime here.
-        //
-        // This is obviously "leaking" memory on each new field, but hey, how many of those
-        // unique renamed fields are you planning to have?
-        match key {
-            Cow::Borrowed(static_key) => self.serialize_struct.serialize_field(static_key, value),
-            Cow::Owned(string_key) => {
-                lazy_static::lazy_static! {
-                    static ref KEYS: Mutex<HashSet<Pin<Box<str>>>> = Mutex::new(HashSet::new());
-                }
-
-                let mut keys = KEYS.lock().unwrap();
-                let boxed_key = Pin::new(string_key.into_boxed_str());
-
-                let static_key: &'static str = match keys.get(&boxed_key) {
-                    Some(existing_boxed_key) => unsafe {
-                        std::mem::transmute::<&str, &'static str>(existing_boxed_key)
-                    },
-                    None => {
-                        let static_key =
-                            unsafe { std::mem::transmute::<&str, &'static str>(&boxed_key) };
-                        keys.insert(boxed_key);
-                        static_key
-                    }
-                };
-
-                self.serialize_struct.serialize_field(static_key, value)
+// Serde expects struct fields to be `&'static str` because for structs the field
+// names are knows at compile time. A serializer can theoretically hold on to those
+// field name references forever and expect them to be valid.
+//
+// To be able to rename a field, we thus need to somehow generate a string at
+// runtime that will have a 'static lifetime.
+//
+// The 'static lifetime is defined as 'will live till the program ends'. Here
+// we keep a static set of pinned Box<str>, and store unique names in it.
+// The static set will not get destroyed until the program ends. Although
+// boxes can move in the set, where they point remains pinned in place until the end
+// of the program because we never delete. For all practical purposes those boxed
+// strs are static, so safe to transmute to 'static lifetime here.
+//
+// This is obviously "leaking" memory on each new field, but hey, how many of those
+// unique renamed fields are you planning to have?
+fn into_static_str(key: Cow<'static, str>) -> &'static str {
+    match key {
+        Cow::Borrowed(static_key) => static_key,
+        Cow::Owned(string_key) => {
+            lazy_static::lazy_static! {
+                static ref KEYS: Mutex<HashSet<Pin<Box<str>>>> = Mutex::new(HashSet::new());
             }
+
+            let mut keys = KEYS.lock().unwrap();
+            let boxed_key = Pin::new(string_key.into_boxed_str());
+
+            let static_key: &'static str = match keys.get(&boxed_key) {
+                Some(existing_boxed_key) => unsafe {
+                    std::mem::transmute::<&str, &'static str>(existing_boxed_key)
+                },
+                None => {
+                    let static_key =
+                        unsafe { std::mem::transmute::<&str, &'static str>(&boxed_key) };
+                    keys.insert(boxed_key);
+                    static_key
+                }
+            };
+
+            static_key
         }
     }
+}
+
+#[test]
+fn test_into_static_str() {
+    // Comparing references here, not content
+    fn assert_refs_eq(left: &str, right: &str) {
+        assert_eq!(left as *const _, right as *const _);
+    }
+
+    fn assert_refs_ne(left: &str, right: &str) {
+        assert_ne!(left as *const _, right as *const _);
+    }
+
+    // Static strings are just pass-through
+    let foo_str: &'static str = "foo";
+    assert_refs_eq(into_static_str(Cow::Borrowed(foo_str)), foo_str);
+    let bar_str: &'static str = "bar";
+    assert_refs_eq(into_static_str(Cow::Borrowed(bar_str)), bar_str);
+
+    // Pass-through, even if the value is repeating
+    let bar_str_again: &'static str = &"_bar"[1..]; // slice shenanigans, to stop compiler from reusing strs.
+    assert_refs_ne(bar_str, bar_str_again);
+    assert_refs_eq(into_static_str(Cow::Borrowed(bar_str_again)), bar_str_again);
+
+    // Owned values are cached
+    let baz_str = "baz";
+    let first_instance: &'static str = into_static_str(Cow::Owned(baz_str.to_string()));
+    assert_refs_ne(baz_str, first_instance);
+
+    // For a repeated owned string a ref to the previous instance is returned
+    assert_refs_eq(
+        into_static_str(Cow::Owned(baz_str.to_string())),
+        first_instance,
+    );
+
+    // Static strings are still pass-through, even if we have cached the exact same
+    // owned one (i.e., we don't want hash lookups)
+    assert_refs_eq(into_static_str(Cow::Borrowed(baz_str)), baz_str);
 }
