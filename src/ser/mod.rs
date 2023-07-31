4dfb7c6ce1fed492c92b42c7f22824e3d119b675
Anatoliy Samara <>
2023-07-31 11:18:14 +0200

    Docs for `ser::hooks`

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -87,9 +87,57 @@ pub enum HooksError {
     IndexNotFound(usize),
 }
 
-pub fn hook<'s, 'h, T: Serialize + ?Sized, H: Hooks>(
+/// Attach serialization hooks to a serializable value.
+///
+/// This function returns a new serializable value (i.e. that is `Serialize`)
+/// that wraps the `serializable` and calls back on the implementation of
+/// [Hooks] on `hooks` at specific moments during serialization.
+///
+/// Serialization hooks allow inspecting and modifying the serialized value
+/// at runtime.
+///
+/// Note that `hooks` must live at least as long as the `serializable`.
+///
+/// # Example:
+/// ```
+/// use serde::Serialize;
+/// use serde_hooks::ser;
+///
+/// #[derive(Serialize)]
+/// struct User {
+///     full_name: String,
+///     password: String,
+/// }
+///
+/// struct UserHooks {
+///     hide_passwords: bool
+/// };
+///
+/// impl ser::Hooks for UserHooks {
+///     // This hook is called on every serialized struct.
+///     fn on_struct(&self, st: &mut ser::StructScope) {
+///         // This is similar to #[serde(rename_all="SCREAMING_SNAKE_CASE")].
+///         st.rename_all_fields_case("SCREAMING_SNAKE_CASE");
+///
+///         // This is similar to #[serde(skip)], but the decision to skip is made at runtime.
+///         if self.hide_passwords {
+///             st.skip_field("password");
+///         }
+///     }
+/// }
+///
+/// let user = User {
+///     full_name: "John Doe".into(),
+///     password: "AKJHDKSHD".into(),
+/// };
+/// let json = serde_json::to_string(&ser::hook(&user, &UserHooks { hide_passwords: true } )).unwrap();
+///
+/// assert_eq!(json, r#"{"FULL_NAME":"John Doe"}"#);
+/// ```
+/// For more examples, check the documentation of [Hooks].
+pub fn hook<'s, 'h: 's, T: Serialize + ?Sized, H: Hooks>(
     serializable: &'s T,
     hooks: &'h H,
-) -> SerializableWithContext<'s, 'h, T, H> {
+) -> impl Serialize + 's {
     SerializableWithContext::new(serializable, hooks)
 }
