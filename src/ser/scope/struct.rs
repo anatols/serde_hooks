8f38c4baed36240a269fad14c013a52ff1085089
Anatoliy Samara <>
2023-08-26 12:14:35 +0200

    Updated docs for `StructScope`

--- src/ser/scope/struct.rs
+++ src/ser/scope/struct.rs
@@ -69,7 +69,6 @@ impl StructScope {
         self
     }
 
-    //TODO better docs - explain about static strings
     /// Rename a field.
     ///
     /// The `key` refers to the original field key in the struct, even if [`rename_all_fields_case`](Self::rename_all_fields_case)
@@ -77,6 +76,10 @@ impl StructScope {
     ///
     /// If you use serde's `#[derive(Serialize)]` and `#[serde(rename=...)]` or
     /// `#[serde(rename_all=...)]`, you need to specify the field key as it will be *after* serde renaming.
+    ///
+    /// Serde expects field names to be known at compile time, and as such, to be static. Passing in a
+    /// borrowed `&'static str` for the new key name here fulfills this. However, passing in
+    /// an owned `String` leads to special handling described in [Static strings](crate#static-strings).
     ///
     /// Returns `self` to allow chaining calls.
     pub fn rename_field(
@@ -100,14 +103,36 @@ impl StructScope {
     /// `#[serde(rename_all=...)]`, those renames will be applied first. See [`Case`](crate::Case) for more information
     /// and caveats of case conversion.
     ///
+    /// Serde expects field names to be known at compile time, and as such, to be static.
+    /// Renaming field names with a case convention will produce strings in runtime,
+    /// which leads to special handling described in [Static strings](crate#static-strings).
+    ///
     /// Returns `self` to allow chaining calls.
     pub fn rename_all_fields_case(&mut self, case: impl Into<Case>) -> &mut Self {
         self.actions.push(StructFieldAction::RenameAll(case.into()));
         self
     }
 
-    //TODO document, explain replacing non-trivial values
+    /// Replace a value for a field.
     ///
+    /// The passed in [`StaticValue`] can represent both primitive and compound value types.
+    ///
+    /// Primitive values are copied, and are later fed to the serializer instead of the original
+    /// struct field values.
+    ///
+    /// For compound values, only metadata is stored, therefore it's not possible to
+    /// serialize the actual values from the contents of [`StaticValue`]. Passing in a
+    /// compound value here would result in an
+    /// [`HooksError::ValueNotSerializable`](crate::ser::HooksError::ValueNotSerializable) error.
+    ///
+    /// The trick to replace a compound value is to replace it in this scope with a primitive one
+    /// (e.g. a unit), subscribe to `on_value` hook, and replace the value there again with the
+    /// compound one.
+    ///
+    /// The replacement value does not necessarily need to be of the same type as the
+    /// original value in the struct. E.g., you can replace an integer field with a string one.
+    /// Some serializers might expect the types to be following a schema, and fail the serialization
+    /// if the replacement value is of a wrong type.
     ///
     /// Returns `self` to allow chaining calls.
     pub fn replace_value(
