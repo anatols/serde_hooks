f272e39e847f61952b9457c354685aad5ec7c2d8
Anatoliy Samara <>
2023-08-26 11:19:20 +0200

    Documented tuple scopes

--- src/ser/scope/tuple.rs
+++ src/ser/scope/tuple.rs
@@ -1,3 +1,11 @@
+/// Inspect tuples.
+///
+/// See [`Hooks::on_tuple`](crate::ser::Hooks::on_tuple),
+/// [`Hooks::on_tuple_variant`](crate::ser::Hooks::on_tuple_variant).
+///
+/// This scope does not allow modifying the tuple elements. However, hooks
+/// for tuples would additionally receive a [`SeqScope`](crate::ser::SeqScope)
+/// that would allow to do so.
 pub struct TupleScope {
     tuple_len: usize,
 }
@@ -7,11 +15,19 @@ impl TupleScope {
         Self { tuple_len }
     }
 
+    /// Returns the tuple length.
     pub fn tuple_len(&self) -> usize {
         self.tuple_len
     }
 }
 
+/// Inspect tuple struct.
+///
+/// See [`Hooks::on_tuple_struct`](crate::ser::Hooks::on_tuple_struct).
+///
+/// This scope does not allow modifying the tuple elements. However, hooks
+/// for tuples would additionally receive a [`SeqScope`](crate::ser::SeqScope)
+/// that would allow to do so.
 pub struct TupleStructScope {
     struct_name: &'static str,
     tuple_len: usize,
@@ -25,10 +41,12 @@ impl TupleStructScope {
         }
     }
 
+    /// Returns the tuple struct name.
     pub fn struct_name(&self) -> &'static str {
         self.struct_name
     }
 
+    /// Returns the tuple length.
     pub fn tuple_len(&self) -> usize {
         self.tuple_len
     }
