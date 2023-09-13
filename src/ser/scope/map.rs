1d101af71371f31ebdfb31d2ae7f4a31c7cd8751
Anatoliy Samara <>
2023-09-13 10:15:04 +0200

    Some docs and cleanup for `MapScope`

--- src/ser/scope/map.rs
+++ src/ser/scope/map.rs
@@ -9,6 +9,10 @@ use crate::{
 //TODO tests for everything
 //TODO add support for rename_key_case and rename_all_keys_case
 //TODO add support for add_entry_before, add_entry_after, push_entry
+
+/// Inspect maps and modify their contents.
+///
+/// See [`Hooks::on_map`](crate::ser::Hooks::on_map).
 pub struct MapScope {
     map_len: Option<usize>,
     actions: MapEntryActions,
@@ -26,17 +30,50 @@ impl MapScope {
         self.actions
     }
 
+    /// Returns the original number of entries in this map, if known.
+    ///
+    /// This is a hint that the serializer gets from the map's `Serialize` implementation
+    /// if the map knows the number of items in it at runtime.
+    ///
+    /// The returned value is not affected by any retain or skip actions.
     pub fn map_len(&self) -> Option<usize> {
         self.map_len
     }
 
-    pub fn retain_entry(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
-        self.actions.push(MapEntryAction::Retain(key.into()));
+    /// Skips an entry during serialization.
+    ///
+    /// This is similar to `#[serde(skip)]` or `#[serde(skip_serializing)]`, but
+    /// works for maps.
+    ///
+    /// At the moment this hook is called it is impossible to predict which
+    /// fields will actually be fed to the serializer afterwards. Therefore, it's not
+    /// possible to correctly adjust the length hint. The underlying serializer will thus
+    /// be given `None` as the map length hint if you call this method. Some serializers
+    /// might not support this.
+    ///
+    /// Returns `self` to allow chaining calls.
+    pub fn skip_entry(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
+        self.actions.push(MapEntryAction::Skip(key.into()));
         self
     }
 
-    pub fn skip_entry(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
-        self.actions.push(MapEntryAction::Skip(key.into()));
+    /// Retains an entry.
+    ///
+    /// Calling this method switches processing to a 'retain' mode, in which
+    /// all not retained entries are skipped. You can retain multiple entries by
+    /// calling this method multiple times.
+    ///
+    /// You can see this as a 'whitelist' counterpart of [`skip_entry`].
+    ///
+    /// At the moment this hook is called it is impossible to predict which
+    /// fields will actually be fed to the serializer afterwards. Therefore, it's not
+    /// possible to correctly adjust the length hint. The underlying serializer will thus
+    /// be given `None` as the map length hint if you call this method. Some serializers
+    /// might not support this.
+    ///
+    /// Returns `self` to allow chaining calls.
+    pub fn retain_entry(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
+        self.actions.push(MapEntryAction::Retain(key.into()));
         self
     }
 
@@ -47,12 +84,6 @@ impl MapScope {
     ) -> &mut Self {
         self.actions
             .push(MapEntryAction::Add(key.into(), Some(value.into())));
-        self
-    }
-
-    //TODO is this needed at all?
-    pub fn add_entry_on_serialize(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
-        self.actions.push(MapEntryAction::Add(key.into(), None));
         self
     }
 
@@ -68,16 +99,6 @@ impl MapScope {
         self
     }
 
-    //TODO is this needed at all?
-    pub fn add_or_replace_entry_on_serialize(
-        &mut self,
-        key: impl Into<MapKeySelector>,
-    ) -> &mut Self {
-        self.actions
-            .push(MapEntryAction::ReplaceOrAdd(key.into(), None));
-        self
-    }
-
     pub fn replace_value(
         &mut self,
         key: impl Into<MapKeySelector>,
@@ -85,12 +106,6 @@ impl MapScope {
     ) -> &mut Self {
         self.actions
             .push(MapEntryAction::Replace(key.into(), Some(new_value.into())));
-        self
-    }
-
-    //TODO is this needed at all?
-    pub fn replace_value_on_serialize(&mut self, key: impl Into<MapKeySelector>) -> &mut Self {
-        self.actions.push(MapEntryAction::Replace(key.into(), None));
         self
     }
 
